97db93b6a73a87b6577f9545ecd6d265671981e1
carlosalbferreira <carlinegaby@gmail.com>
2024-07-09 12:12:46 -0300

    Primeiro Commit Introdução ao JS, Tipos Primitivos, Operadores e Atribuição.

---
+++ Javascript-Angular-Udemy/003-Operadores-aritmeticos/005-Atribuicao/index.js
@@ -0,0 +1,21 @@
+let valor = 3;
+let valorB = 1;
+// console.log(valor + valorB +" Soma o valor das Variáveis");
+
+// console.log("**************");
+
+// valor +=1; //Incrementa Valores
+// console.log(valor + "Incrementa");
+
+// valor -=1; //Decrementa Valores
+// console.log(valor + "Decrementa");
+
+// valor *=2;
+// console.log(valor + "Multiplica");
+
+valor /=6;
+console.log(valor + "Divide ");
+/**
+ *  =, +=, -=, *=.
+ */
+
