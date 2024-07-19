4a92eb6e52277c32d4288669de14bdd017f45f5c
carlosalbferreira <carlinegaby@gmail.com>
2024-07-19 20:25:00 -0300

    Funcoes => Arrow function e expressoes de funcoes

--- Javascript-Angular-Udemy/007-Funcoes/004-Function-X-ArrowFunction/index.js
+++ Javascript-Angular-Udemy/007-Funcoes/004-Function-X-ArrowFunction/index.js
@@ -5,9 +5,49 @@
  * 4 - Arguments
  */
 
+function nomefunc(){
+    return "Carlos Alberto";
+}
+console.log(nomefunc());
+
+const nomeFuncArrow = () => {
+    return "Carlos Alberto";
+}
+console.log(nomeFuncArrow());
+
+const nomeFuncArrowRetorno = () => "Carlos Alberto"; // Retorna na mesma linha
+console.log(nomeFuncArrowRetorno());
+
+const nomeFuncArrowHoisting = () => {
+    return "Carlos Alberto";
+}
+console.log(nomeFuncArrowHoisting());// em Arrow Function so podemos chamar a função na ordem.
+
+const nomeArrowParams = (param) => param;
+console.log(nomeArrowParams("Parametro"));
+// Não funciona o Arguments
+// const nomeFuncArrowArguments = () => {
+//     return arguments;
+// }
+// console.log(nomeFuncArrowArguments());
+
+function nomeFuncArguments() {
+    return arguments;
+}
+
+console.log(nomeFuncArguments("Carlos Alberto"));
+
 /**
- * 5 - Não pode ser invocada com new
+ * 5 - Funções não podem ser invocadas com new porque não tem constructor.
  */
+class newFunc{
+    constructor(nome){
+        this.nome = nome;
+    }
+}
+const a = new newFunc("Carlos Alberto");
+console.log(a.nome);
+
 
 /**
  * 6 - Contexto
@@ -20,9 +60,48 @@
  * Se ele não tiver associado a ninguem na chamada, ele assumira o This automaticamente
  * como o contexto global, que no caso o navegador é window.
  */
+const lanches = {
+
+    cardapio: [
+        {nome: "X-Salada", preco: "R$ 25"},
+        {nome: "X-Bacon ", preco: "R$ 30"},
+    ],
+
+    meuPedidoFunc: function(select) {
+        return console.log(this.cardapio[select]);
+    },
+
+    meuPedidoFuncTimeOut: function(){
+        setTimeout(function() {
+            console.log(this.cardapio);
+            console.log(this);
+        }.bind(this), 1000); // utiliza o .bind(this) para entrar dentro do objeto pai.
+    },
+
+    meuPedidoArrow: (select) => {
+        this.cardapio = [
+            {nome: "X-Salada", preco: "R$ 25"},
+            {nome: "X-Bacon ", preco: "R$ 30"},
+        ]
+        return console.log(this.cardapio[select]);
+    }
+}
+
+lanches.meuPedidoFunc(1);
+lanches.meuPedidoArrow(0);
+lanches.meuPedidoFuncTimeOut(1);
+
 
 /**
  * 7 -  Constructor 
  * Arrow Functions não podem ser constructors, então nao é possivel usar o operador 
  * new com a mesma.
  */
+
+class newFunc{
+    constructor(nome){
+        this.nome = nome;
+    }
+}
+const teste = new newFunc("Carlos Alberto");
+console.log(teste.nome);
---
+++ Javascript-Angular-Udemy/007-Funcoes/005- IIFE-Expressoes-de-funcoes-invocadas-imediatamente/index.js
@@ -0,0 +1,16 @@
+// IIFE Immediately Invoked Function Expression .
+// IIFE Expressão de Função Invocada Imeditamente.
+
+(function(){
+    return console.log('con');
+})();
+
+
+(function(num1,  num2){
+    const soma = num1 + num2;
+    return console.log(soma);
+})(1, 2);
+
+// function consol(con){
+//     return console.log(con);
+// }
