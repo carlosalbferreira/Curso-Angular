4b06e4a562a1587f9d34c63fc7a9f4b964975339
carlosalbferreira <carlinegaby@gmail.com>
2024-08-30 19:34:33 -0300

    Destruct e verificação de existencia e valor dentro de Objects

--- Javascript-Angular-Udemy/008-Objetos-a-fundo/004-Destructuring/index.js
+++ Javascript-Angular-Udemy/008-Objetos-a-fundo/004-Destructuring/index.js
@@ -2,16 +2,23 @@ const Tenis = {
     tamanho: 45,
     estoque: true,
     preco: 199.90,
-    marca: [
-        {
-            nome: "Nike",
-        },
-        {
-            nome: "Adidas",
-        },
-    ],
+    marca: [{nome: "Nike",}, {nome: "Adidas",},],
+    secret: 123456,
+    n: 5,
+    link: {a: "a é == a", b: {c: "c == c"}},
 };
 
-const {tamanho, estoque, preco, marca} = Tenis;
+// const {tamanho, estoque, preco, marca = "Não possui MARCA"} = Tenis;
 
-console.log(tamanho, estoque, preco, marca);
+// console.log(tamanho, estoque, preco, marca);
+
+// // podemos criar ALIAS dentro do nosso destruct
+
+// const {secret: randonNumber, n: avaliacoes} = Tenis;
+
+// console.log(randonNumber);
+// console.log(avaliacoes);
+
+const {link: {a, b: {c}}} = Tenis; //Podemos acessar os objetos em niveis.
+
+console.log(c);
---
+++ Javascript-Angular-Udemy/008-Objetos-a-fundo/005-Verificacao-de-existencia-de-propriedade-e-valor/index.js
@@ -0,0 +1,15 @@
+const Tenis = {
+    tamanho: 45,
+    estoque: true,
+    preco: 199.90,
+    marca: [{nome: "Nike",}, {nome: "Adidas",},],
+    secret: 123456,
+    n: 5,
+    link: {a: "a é == a", b: {c: "c == c"}},
+};
+
+//hasOwnPropety | propetyName in tenis
+
+const {link: {a, b: {c}} } = Tenis
+
+console.log(a , c);
