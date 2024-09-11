1843045b6d1d87f0bf21ed31a1de510cac9dc7c6
carlosalbferreira <carlinegaby@gmail.com>
2024-09-11 20:26:16 -0300

    Clonagem de objetos rasos e profundos

--- Javascript-Angular-Udemy/008-Objetos-a-fundo/008-Mesclando-e-clonando-objetos/index.js
+++ Javascript-Angular-Udemy/008-Objetos-a-fundo/008-Mesclando-e-clonando-objetos/index.js
@@ -5,11 +5,22 @@ let Tenis = {
 
 let link = {link: {a: "a", b: {c: "c"}}};
 
-let clone1 = Tenis;
+let clone1 = Tenis
+ 
 console.log(clone1);
 
-let mesclar1 = Object.assign(Tenis, link);
+// Faz a união dos objetos para uma variável só.
+let mesclar1 = Object.assign(Tenis, link); // Assing diz que vai juntar os objetos
 console.log(mesclar1);
 
-let mesclar2 = {...Tenis, link};
+let mesclar2 = {...Tenis, ...link};
 console.log(mesclar2);
+
+//Pode haver problemas caso queiramos adicionar itens
+//Ele pode alterar o que você esta clonando por ser a referência.
+
+clone1.estoque = false;
+
+console.log(mesclar1);
+console.log(mesclar2);
+console.log(Tenis);
