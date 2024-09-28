43c8a13847c79310a081b938f94646c6541fcecf
carlosalbferreira <carlinegaby@gmail.com>
2024-09-27 21:30:57 -0300

    Atualizando os Commits

--- Javascript-Angular-Udemy/009-Array-a-fundo/001-o-que-e-array/index.js
+++ Javascript-Angular-Udemy/009-Array-a-fundo/001-o-que-e-array/index.js
@@ -3,3 +3,7 @@
  *  0 - Lista de Dados
  *  1 - Conjunto de Dados
  */
+
+
+ console.log([1, 2, 3, true, 'Carlos', {nome: "Carlos Alberto"}]);
+ 
---
+++ Javascript-Angular-Udemy/009-Array-a-fundo/002-Indices-e-elementos/index.js
@@ -0,0 +1,12 @@
+//indices sempre iniciam em 0
+
+
+
+const arr = [1, 2, 3, 4, true, 'Carlos', "Alberto"];
+
+//table mostra o array e todos os seus indices (index)
+console.table(arr);
+console.log(arr[4]); //escolho pelo indice qual elemento do array eu quero
+
+//.lenght mostra quantos elementos o array possui.
+console.log(arr.length);
