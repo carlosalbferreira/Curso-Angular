e640e23405502275502e316705952d43eb9b6f08
carlosalbferreira <carlinegaby@gmail.com>
2024-12-13 23:29:24 -0300

    Removendo manipuladores

--- Javascript-Angular-Udemy/011-Eventos/004-Removendo-manipuladores-de-eventos/index.js
+++ Javascript-Angular-Udemy/011-Eventos/004-Removendo-manipuladores-de-eventos/index.js
@@ -3,8 +3,11 @@
 const btn = document.querySelector('button');
 
 const acionarAlert = () =>{
+    let validador = false;
     alert('Carlos Clicou !');
+    validador = true;
+    if (validador) {
+        btn.removeEventListener('click', acionarAlert);
+    }
 }
-
 btn.addEventListener('click', acionarAlert);
-btn.removeEventListener('click', acionarAlert);
