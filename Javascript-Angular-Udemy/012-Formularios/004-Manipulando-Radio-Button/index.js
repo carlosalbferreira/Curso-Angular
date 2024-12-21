5403cf10f28c4c9e50acae58c06b4b7b064addda
carlosalbferreira <carlinegaby@gmail.com>
2024-12-21 19:52:43 -0300

    Manipulando radio button estilizando com javascript

--- Javascript-Angular-Udemy/012-Formularios/004-Manipulando-Radio-Button/index.js
+++ Javascript-Angular-Udemy/012-Formularios/004-Manipulando-Radio-Button/index.js
@@ -5,7 +5,28 @@ const form = document.forms.namedItem("select-radio");
 form.addEventListener("submit", (event) =>{
     event.preventDefault();
     
-    const checkbox = form.checkbox;
+    const radio = form.radio;
+    console.log(radio.value);
 
-    console.log(checkbox);
+
+
+    if(radio.value == 'html5'){
+        radio.value = document.querySelector('.label-html').style.background = 'green'
+    }else{
+        radio.value = document.querySelector('.label-html').style.background = 'white'
+    }
+
+    if(radio.value == 'css3'){
+        radio.value = document.querySelector('.label-css').style.background = 'green'
+    }else{
+        radio.value = document.querySelector('.label-css').style.background = 'white'
+    }
+
+    if(radio.value == 'javascript'){
+        radio.value = document.querySelector('.label-js').style.background = 'green'
+    }else{
+        radio.value = document.querySelector('.label-js').style.background = 'white'
+    }
+
 })
+
